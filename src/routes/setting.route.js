b7aa8baed6757de31bf0717ee204ac5e1f653dcd
DESKTOP-UVL41M2\i7 <kpaparid@gmail.com>
2022-03-31 05:34:10 +0200

    no message

--- src/routes/setting.route.js
+++ src/routes/setting.route.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 var SettingController = require("../controllers/setting.controller");
 
-// router.get("/", SettingController.getSettings);
-// router.post("/", SettingController.createSetting);
+router.get("/", SettingController.getSettings);
+router.post("/", SettingController.createSetting);
 router.delete("/:id", SettingController.deleteSetting);
-// router.put("/:id", SettingController.updateSetting);
+router.put("/:id", SettingController.updateSetting);
 
 module.exports = router;
