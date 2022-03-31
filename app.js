22ea16fd29bd3a9e75152902c5f89b5c4c8929ba
DESKTOP-UVL41M2\i7 <kpaparid@gmail.com>
2022-03-31 04:51:05 +0200

    no message

--- app.js
+++ app.js
@@ -1,68 +1,69 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
-require("dotenv").config({ path: "./.env.local" });
+// const { default: mongoose } = require("mongoose");
+// require("dotenv").config({ path: "./.env.local" });
 // const app = express();
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 const port = process.env.PORT || 3005;
-const cors = require("cors");
+// const cors = require("cors");
 
-const meal_routes = require("./src/routes/meal.route.js");
-const setting_routes = require("./src/routes/setting.route.js");
-const order_routes = require("./src/routes/order.route.js");
+// const meal_routes = require("./src/routes/meal.route.js");
+// const setting_routes = require("./src/routes/setting.route.js");
+// const order_routes = require("./src/routes/order.route.js");
 
 const app = express();
-// app.listen(port, () => {
-//   console.log("Server has started!");
-// });
+app.listen(port, () => {
+  console.log("Server has started!");
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.set("port", port);
-app.use(express.json());
-app.use(cors());
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Database Connected");
-    app.use("/meals", meal_routes);
-    app.use("/settings", setting_routes);
-    app.use("/orders", order_routes);
 
-    app.use("*", (req, res) => {
-      return res.status(404).json({
-        success: false,
-        message: "API endpoint doesnt exist",
-      });
-    });
+// app.set("port", port);
+// app.use(express.json());
+// app.use(cors());
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("Database Connected");
+//     app.use("/meals", meal_routes);
+//     app.use("/settings", setting_routes);
+//     app.use("/orders", order_routes);
 
-    const server = require("http").createServer(app);
-    const io = new Server(server, {
-      cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-      },
-    });
-    io.on("connection", (socket) => {
-      console.log("user connected", socket.id);
-      socket.off("disconnect", () => {
-        console.log("User Disconnected", socket.id);
-        // socket.off();
-      });
-      socket.on("join_room", (data) => {
-        console.log(`${socket.id} joined room ${data}`);
-        socket.join(data);
-      });
-      socket.on("send_order", (data) => {
-        console.log(`${socket.id} sending order ${data}`);
-        socket.to("admin").emit("received_order", data);
-        // io.sockets
-      });
-    });
+//     app.use("*", (req, res) => {
+//       return res.status(404).json({
+//         success: false,
+//         message: "API endpoint doesnt exist",
+//       });
+//     });
 
-    server.listen(port);
-    server.on("listening", () => {
-      console.log(`Listening on port:: http://localhost:${port}/`);
-    });
-  })
+//     const server = require("http").createServer(app);
+//     const io = new Server(server, {
+//       cors: {
+//         origin: "http://localhost:3000",
+//         methods: ["GET", "POST"],
+//       },
+//     });
+//     io.on("connection", (socket) => {
+//       console.log("user connected", socket.id);
+//       socket.off("disconnect", () => {
+//         console.log("User Disconnected", socket.id);
+//         // socket.off();
+//       });
+//       socket.on("join_room", (data) => {
+//         console.log(`${socket.id} joined room ${data}`);
+//         socket.join(data);
+//       });
+//       socket.on("send_order", (data) => {
+//         console.log(`${socket.id} sending order ${data}`);
+//         socket.to("admin").emit("received_order", data);
+//         // io.sockets
+//       });
+//     });
 
-  .catch((e) => console.log("Database Error " + e));
+//     server.listen(port);
+//     server.on("listening", () => {
+//       console.log(`Listening on port:: http://localhost:${port}/`);
+//     });
+//   })
+
+//   .catch((e) => console.log("Database Error " + e));
